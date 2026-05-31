657629355766cfce68c2ea43e6239d215a95b92b
mahmoudhaddad <mahmoud.e.alhaddad@gmail.com>
2026-05-31 23:00:00 +0300

    edits

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: ['api/*']);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
